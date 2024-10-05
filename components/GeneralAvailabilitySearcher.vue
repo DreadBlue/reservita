bfac8b749ece5bb0d0a4e513e48cb5ac61a77943
DreadBlue <desarrollo@dreadblue.com>
2024-10-05 14:32:32 -0500

    ServerFixed

--- components/GeneralAvailabilitySearcher.vue
+++ components/GeneralAvailabilitySearcher.vue
@@ -2,7 +2,7 @@
   <v-container fluid class="bg-mywhite rounded-lg">
     <v-row>
       <v-col cols="12" class="d-flex align-center ga-10">
-        <!-- <general-date-picker
+        <general-date-picker
           v-model="dateValue"
           label="Día de actividad"
           style="max-width: 250px"
@@ -15,26 +15,26 @@
           :disabled="!dateValue"
           @click="redirection"
           >Ver disponibilidad</v-btn
-        > -->
+        >
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-// import dayjs from "dayjs";
-// import customParseFormat from "dayjs/plugin/customParseFormat";
-// import { useBookingStore } from "@/stores/booking";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { useBookingStore } from "@/stores/booking.js";
 
-// const bookingStore = useBookingStore();
-// const maxDate = ref(bookingStore.max);
-// const router = useRouter();
-// dayjs.extend(customParseFormat);
+const bookingStore = useBookingStore();
+const maxDate = ref(bookingStore.max);
+const router = useRouter();
+dayjs.extend(customParseFormat);
 
 const dateValue = ref();
 
-// const redirection = () => {
-//   const dateFormatted = dayjs(dateValue.value, "DD-MM-YYYY").format("YYYY-MM-DD");
-//   router.push({ path: "/reservar", query: { date: dateFormatted } });
-// };
+const redirection = () => {
+  const dateFormatted = dayjs(dateValue.value, "DD-MM-YYYY").format("YYYY-MM-DD");
+  router.push({ path: "/reservar", query: { date: dateFormatted } });
+};
 </script>
