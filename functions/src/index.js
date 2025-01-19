791bd0d26e6697df591c03725def217f11344f47
DreadBlue <desarrollo@dreadblue.com>
2025-01-19 17:08:51 +0100

    Booking process finished

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,7 +2,11 @@ const { getAvailability, makeReservation, lookBooking } = require('./booking');
 const { sendEmail,
   // sendCalendar
 } = require('./google.js');
+<<<<<<< HEAD
 // const { paymentReceiver } = require('./payments.js');
+=======
+// const { generateHash, paymentReceiver } = require('./payments.js');
+>>>>>>> f2ae2d0 (Booking process finished)
 const { manualBill, monthlyBill, firestoreTesting } = require('./dreadblue.js');
 // const { deleteBooking, createDatabase, adminBookings, createRequest, fetchRequests, manageChangeRequest } = require('./admin.js');
 const { createDatabase, adminBookings, deleteBooking } = require('./admin.js');
@@ -15,7 +19,11 @@ module.exports = {
   makeReservation,
   monthlyBill,
   manualBill,
+<<<<<<< HEAD
   deleteBooking,
+=======
+  // deleteBooking,
+>>>>>>> f2ae2d0 (Booking process finished)
   createDatabase,
   adminBookings,
   // createRequest,
